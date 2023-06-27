37901ab12be66e99b7d2d8b41a6cb500b642e536
David <dzhang.dz64@gmail.com>
2023-06-27 09:15:13 -0500

    se modifica jugador

--- BattleShip.java
+++ BattleShip.java
@@ -9,7 +9,7 @@ public class BattleShip {
         
         int x,y,t;
 
-        obj.Tablero(obj.table1);
+        obj.Tablero(obj.table1);//llamado al metodo para crear tablero
         for(int i=0;i<2;i++){
             System.out.println("inserte las coordenadas de la fila");
             x=Integer.parseInt(libro.readLine());
@@ -17,16 +17,16 @@ public class BattleShip {
             y=Integer.parseInt(libro.readLine());
             System.out.println("inserte tamaño del barco");
             t=Integer.parseInt(libro.readLine());
-            obj.BarcoH(obj.table1,t,x,y);
+            obj.BarcoV(obj.table1,t,x,y);//llamado al metodo para guardar barco en horizontal
         }
-        obj.MostrarTablero(obj.table1);
+        obj.MostrarTablero(obj.table1);//llamado al metodo para mostrar tablero
 
         System.out.println("inserte x");
         x=Integer.parseInt(libro.readLine());
         System.out.println("inserte y");
-        y=Integer.parseInt(libro.readLine());
-        obj.Ataque(obj.table1,x,y);
-        obj.MostrarTablero(obj.table1);
+        y=Integer.parseInt(libro.readLine()); 
+        obj.Ataque(obj.table1,x,y);//llamado al metodo para atacar
+        obj.MostrarTablero(obj.table1);//llamado al metodo para mostrar tablero
         
     }
 }
