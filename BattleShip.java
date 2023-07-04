34950a2ea3e03e94d475778416988ab7d411c28e
Martin <martingomez3696@gmail.com>
2023-07-03 19:54:16 -0500

    se arreglaron los porblemas

--- BattleShip.java
+++ BattleShip.java
@@ -1,40 +1,24 @@
 import java.io.*;
-
 public class BattleShip {
     static BufferedReader libro=new BufferedReader(new InputStreamReader(System.in));
     static int x,y,t,opc_barco=0;
     public static void main(String[] args) throws IOException {
 
-    
         //aca se pondra todo el codigo principal del proyecto
         Jugador player =new Jugador();
         coordenadas coor = new coordenadas();
-        
+
         int barco=0;
         String player1, player2;
         String dato="";
         Boolean fin=false;
         /* 
-
-    BufferedReader libro=new BufferedReader(new InputStreamReader(System.in));
-        //aca se pondra todo el codigo principal del proyecto
-        Jugador obj =new Jugador();
-        
-        int x,y,t;
-        obj.Tablero(obj.table1);//llamado al metodo para crear tablero
-        /*String player1, player2;
-        String dato="";
-        Boolean fin=false;
-        prueba desde afuera 3
-
-
         //se ingresan los nombres de los jugadoes
         System.out.println("---BATTLESHIPS---");
         System.out.println("Ingrese jugador 1:");
         player1 = libro.readLine();
         System.out.println("Ingrese jugador 2:");
         player2 = libro.readLine();
-
         //inicia el juego
         while(!fin){
             System.out.println("\n--Turno de "+player1+"--");
@@ -49,7 +33,6 @@ public class BattleShip {
                 fin=true;
             }
         }*/
-
         player.Tablero(player.table1);//llamado al metodo para crear tablero
 
         //Barco
@@ -102,36 +85,11 @@ public class BattleShip {
         obj.MostrarTablero(obj.table1);//llamado al metodo para mostrar tablero
         */
     }
-
     public static void coordenadas()throws IOException{
         System.out.println("inserte la coordenada en x");
                         x=Integer.parseInt(libro.readLine());
                     System.out.println("inserte la coordenada en y");
                         y=Integer.parseInt(libro.readLine());
-                    
-
-
-        obj.Tablero(obj.table1);
-         //prueba 2
-
-        for(int i=0;i<2;i++){
-            System.out.println("inserte las coordenadas de la fila");
-            x=Integer.parseInt(libro.readLine());
-            System.out.println("inserte las coordenadas de la columna");
-            y=Integer.parseInt(libro.readLine());
-            System.out.println("inserte tamaño del barco");
-            t=Integer.parseInt(libro.readLine());
-            obj.BarcoV(obj.table1,t,x,y);//llamado al metodo para guardar barco en horizontal
-        }
-        obj.MostrarTablero(obj.table1);//llamado al metodo para mostrar tablero
-
-        System.out.println("inserte x");
-        x=Integer.parseInt(libro.readLine());
-        System.out.println("inserte y");
-        y=Integer.parseInt(libro.readLine()); 
-        obj.Ataque(obj.table1,x,y);//llamado al metodo para atacar
-        obj.MostrarTablero(obj.table1);//llamado al metodo para mostrar tablero
-        
 
     }
 }
