3b5c5e9579cf9183d2fdba2d7c5c83221e43017f
Martin <martingomez3696@gmail.com>
2023-07-04 16:08:15 -0500

    se agrega el coordenadas

--- coordenadas.java
+++ coordenadas.java
@@ -1,20 +1,12 @@
 import java.io.*;
-public class Coordenadas {
-        BufferedReader libro = new BufferedReader(new InputStreamReader(System.in));
-        String player1, player2;
-        String dato="";
-        int x, y, i, t;
-        boolean valido1, valido2;
-        Boolean fin=false;
-        String[]coordenadas1=new String[4];//arreglo que guarda las coordenadas
-        String[]coordenadas2=new String[4];
 
-    public int coordenadasy(String coordenada[],int i) throws IOException{
+public class Coordenadas {
+    public int coordenadasy(String dato) throws IOException{
         int num;
         int y;
         try{
-            num=Integer.parseInt(coordenada[i].substring(1));
-            if (num>0 && num<11){
+            num=Integer.parseInt(dato.substring(1));
+            if (0<num && num<11){
                 y=num-1;
             }else{
                 y=-1;
@@ -25,43 +17,33 @@ public class Coordenadas {
         return y;
     }
 
-   public int coordenadasx(String coordenada [], int i){
+    public int coordenadasx(String dato){
         String letra;
         int x;
         letra=dato.substring(0, 1);
         if(letra.equals("A") || letra.equals("a")){
-                x=0;
+            x=0;
         }else if(letra.equals("B") || letra.equals("b")){
-                x=1;
+            x=1;
         }else if(letra.equals("C") || letra.equals("c")){
-                x=2;
+            x=2;
         }else if(letra.equals("D") || letra.equals("d")){
-                x=3;
+            x=3;
         }else if(letra.equals("E") || letra.equals("e")){
-                x=4;
+            x=4;
         }else if(letra.equals("F") || letra.equals("f")){
-                x=5;
+            x=5;
         }else if(letra.equals("G") || letra.equals("g")){
-                x=6;
+            x=6;
         }else if(letra.equals("H") || letra.equals("h")){
-                x=7;
+            x=7;
         }else if(letra.equals("I") || letra.equals("i")){
-                x=8;
+            x=8;
         }else if(letra.equals("J") || letra.equals("j")){
-                x=9;
+            x=9;
         }else{
-                x=-1;
+            x=-1;
         }
         return x;
     }
-    
-    public String[] coor(String[]coordenada,int i) throws IOException{
-        System.out.println("Inserte coordenadas");
-        coordenada[i]=libro.readLine();
-        x=coordenadasx(coordenada,i);
-        y=coordenadasy(coordenada,i);
-        return coordenada;
-    }
-
 }
-
