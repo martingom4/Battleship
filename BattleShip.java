a670156dda610d4d632843f12332f76698bea86f
Martin <martingomez3696@gmail.com>
2023-07-04 23:32:26 -0500

    Se agrega cls en y se va creando la clase de disparo

--- BattleShip.java
+++ BattleShip.java
@@ -7,36 +7,43 @@ public class BattleShip {
         Jugador player =new Jugador();
         Coordenadas c = new Coordenadas();
         Barco boat = new Barco();
-        
         Boolean barco=false;
         Boolean fin=false;
+        String [][] t = new String[9][9];
         int opc_barco=0;
-
         player.Tablero(player.table1);//llamado al metodo para crear tablero del jugador 1
         player.Tablero(player.table2);//llamado al metodo para crear tablero del jugador 2
         player.Nombres();
         
         //Barco jugador 1
         System.out.println("Jugador 1: "+player.player1);
+        Limpiar.clean();
         for(int i=0;i<4;i++){
             
                 System.out.println("Colocar barco "+(i+1)+": \n1-Horizontal(hacia a la derecha)\n2-Vertical(hacia abajo)");
                 opc_barco=Integer.parseInt(libro.readLine());
+                Limpiar.clean();
                 barco=false;
             switch(opc_barco){
                 case 1:
+                        
                         boat.size1=boat.TamañoBarco(boat.ilustracion1,boat.size1,i,boat.nbarco1,boat.barco1);//metodo encontrar el tamaño del barco
-                        while(!barco){
+                        Limpiar.clean();
+                        while(!barco){ 
+                            player.MostrarTablero(player.table1);        
                             c.coor(c.coordenadas1,i);//Llamado al metodo de pedir coordenadas
                             barco=player.BarcoH(player.table1,boat.size1,c.x,c.y,i,boat.direccion1);//llamado al metodo para guardar barco en horizontal
                             player.MostrarTablero(player.table1);//llamado al metodo para mostrar tablero
+                            
                         }
                     break;
-                case 2:
-                        boat.size1=boat.TamañoBarco(boat.ilustracion1,boat.size1,i,boat.nbarco1,boat.barco1);
+                case 2:                     
+                        boat.size1=boat.TamañoBarco(boat.ilustracion1,boat.size1,i,boat.nbarco1,boat.barco1);  
+                        Limpiar.clean();                      
                         while(!barco){
+                            player.MostrarTablero(player.table1);                           
                             c.coor(c.coordenadas1,i);//Llamado la metodo de pedir coordenadas
-                            barco=player.BarcoV(player.table1,boat.size1,c.x,c.y,i,boat.direccion1);//llamado al metodo para guardar barco en horizontal
+                            barco=player.BarcoV(player.table1,boat.size1,c.x,c.y,i,boat.direccion1);//llamado al metodo para guardar barco en vertical 
                             player.MostrarTablero(player.table1);//llamado al metodo para mostrar tablero
                         }
                     break;
@@ -51,14 +58,17 @@ public class BattleShip {
         //barco jugador 2
         System.out.println("Jugador 2: "+player.player2);
         for(int i=0;i<4;i++){
-            
+
                 System.out.println("Colocar barco "+(i+1)+": \n1-Horizontal(hacia a la derecha)\n2-Vertical(hacia abajo)");
                 opc_barco=Integer.parseInt(libro.readLine());
+                Limpiar.clean();
                 barco=false;
             switch(opc_barco){
                 case 1:
                         boat.size2=boat.TamañoBarco(boat.ilustracion2,boat.size2,i,boat.nbarco2,boat.barco2);//metodo encontrar el tamaño del barco
+                        Limpiar.clean();
                         while(!barco){
+                            player.Tablero(player.table2);
                             c.coor(c.coordenadas2,i);//Llamado al metodo de pedir coordenadas
                             barco=player.BarcoH(player.table2,boat.size2,c.x,c.y,i,boat.direccion2);//llamado al metodo para guardar barco en horizontal
                             player.MostrarTablero(player.table2);//llamado al metodo para mostrar tablero
@@ -66,7 +76,9 @@ public class BattleShip {
                     break;
                 case 2:
                         boat.size2=boat.TamañoBarco(boat.ilustracion2,boat.size2,i,boat.nbarco2,boat.barco2);//metodo encontrar el tamaño del barco
+                        Limpiar.clean();
                         while(!barco){
+                            player.Tablero(player.table2);
                             c.coor(c.coordenadas2,i);//Llamado al metodo de pedir coordenadas
                             barco=player.BarcoV(player.table2,boat.size2,c.x,c.y,i,boat.direccion2);//llamado al metodo para guardar barco en horizontal
                             player.MostrarTablero(player.table2);//llamado al metodo para mostrar tablero
