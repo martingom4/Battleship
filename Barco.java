986279e7a70eb740b6e40fab8949412312688afc
David <dzhang.dz64@gmail.com>
2023-07-08 05:50:33 -0500

    reduciendo variables y agregando mas objetos

--- Barco.java
+++ Barco.java
@@ -2,17 +2,16 @@ import java.io.*;
 public class Barco {
     BufferedReader libro = new BufferedReader(new InputStreamReader(System.in));
 
-    int nbarco1[]={2,1,1};
-    int nbarco2[]={2,1,1};
-    int barco1[]={2,3,4};
-    int barco2[]={2,3,4};
-    String[] ilustracion1={"00","000","0000"};
-    String[] ilustracion2={"00","000","0000"};
-    String[] direccion1=new String[4];//arrglo que guarda la direccion Horizontal o vertical del jugador 1
-    String[] direccion2=new String[4];
-    int[] size1=new int[4]; // arreglo que guarda los tamños de los barcos del jugador 1
-    int[] size2=new int[4];
-
+    int nbarco[]={2,1,1};
+    int barco[]={2,3,4};
+    String[] ilustracion={"00","000","0000"};
+    String[] direccion=new String[4];//arrglo que guarda la direccion Horizontal o vertical del jugador 1
+    int[] size=new int[4]; // arreglo que guarda los tamños de los barcos del jugador 1
+    String[]coordenadasbarco=new String[4];
+    String[]coordenadasdisp=new String[4];
+    String dato="";
+    int x, y, i, t;
+    
 //Metodo tamano de barco
 public int[] TamañoBarco(String[] ilus,int[]tam,int i,int[]nbarco,int[]barco)throws IOException{
         Boolean b=true;
@@ -37,13 +36,109 @@ public int[] TamañoBarco(String[] ilus,int[]tam,int i,int[]nbarco,int[]barco)th
         }
         return tam;
     }
+    public int coordenadasy(String coordenada[], int i) throws IOException {
+        int num;
+        int y;
+        try {
+            if (coordenada[i] != null) {  // Verificar si la cadena no es nula
+                num = Integer.parseInt(coordenada[i].substring(1));
+                if (num > 0 && num < 11) {
+                    y = num - 1;
+                } else {
+                    y = -1;
+                }
+            } else {
+                y = -1;
+            }
+        } catch (Exception e) {
+            y = -1;
+        }
+        return y;
+    }
+        public int coordenadasx(String coordenada[],int i){
+            String letra;
+            int x;
+            letra=coordenada[i].substring(0, 1).toUpperCase();
+            switch(letra){
+                case "A":
+                    x=0;
+                break;
+                
+                case "B":
+                    x=1;
+                break;
+                
+                case "C":
+                    x=2;
+                break;
+                
+                case "D":
+                    x=3;
+                break;
+                
+                case "E":
+                    x=4;
+                break;
+                
+                case "F":
+                    x=5;
+                break;
+                
+                case "G":
+                    x=6;
+                break;
+                
+                case "H":
+                    x=7;
+                break;
+                
+                case "I":
+                    x=8;
+                break;
+                
+                case "J":
+                    x=9;
+                break;
+                
+                default:
+                    x=-1;
+                break;
+            }
+            return x;
+        }
+        public String[] coor(String[]coordenada,int i) throws IOException{
+            System.out.println("Inserte coordenadas");
+            coordenada[i]=libro.readLine();
+            x=coordenadasx(coordenada,i);
+            y=coordenadasy(coordenada,i);
+            return coordenada;
+        }
+    
+        public void Disparar(String[][] table,String[][] table_d, String[] coordenada, int i)throws IOException{
+            int x = coordenadasx(coordenada,i);
+            int y = coordenadasy(coordenada,i );
+    
+            if (x >= 0 && x < table.length && y >= 0 && y < table[0].length) {
+                if (table[x][y].equals("-")) {
+                    System.out.println("Agua");
+                    table_d[x][y] = "f";
+                } else if (table[x][y].equals("X")){
+                    System.out.println("Ya has disparado aquí antes");
+                }else if (table[x][y].equals("0")) {
+                System.out.println("Has impactado un barco");
+                table_d[x][y] = "X"; // Marcar como impacto en un barco con "X"
+                }
+            } else {
+                System.out.println("Coordenadas inválidas");
+            }
+        }
     
     public void barcos(int[]t,String[] coord,String[]sentido){
         for(int i=0;i<4;i++){
-            
-            System.out.println( "TamañBarco: "+t[i]);
-            System.out.println("sus coordenadas:"+coord[i]);
-            System.out.println("su sentido es:"+sentido[i]);
+            System.out.println("Barco "+(i+1));
+            System.out.println( "Tamaño: "+t[i]);
+            System.out.println("Coordenadas:"+coord[i]);
+            System.out.println("sentido:"+sentido[i]);
         }
     }
 }
--- BattleShip.java
+++ BattleShip.java
@@ -10,87 +10,48 @@ public class BattleShip {
     public static void main(String[] args) throws IOException {
         //aca se pondra todo el codigo principal del proyecto
         BufferedReader libro=new BufferedReader(new InputStreamReader(System.in));
-        Jugador player =new Jugador();
-        Coordenadas c = new Coordenadas();
-        Barco boat = new Barco();
         Boolean barco=false;
         Boolean fin=false;
         int turnos;
         int opc_barco=0;
         int i=0;
-        
-        player.Tablero(player.table1);//llamado al metodo para crear tablero del jugador 1
-        player.Tablero(player.table2);//llamado al metodo para crear tablero del jugador 2
-        player.Tablero(player.table3);// se llama al metodo de tablero para disparo del jugador 1
-        player.Tablero(player.table4);// se llama al metodo de tablero para disparo del jugador 2
-        player.Nombres();
-        
+        Jugador player1 = new Jugador(1); //Jugador 1
+        Jugador player2 = new Jugador(2);
+        Barco boat1 = new Barco(); // Barcos del jugador 1
+        Barco boat2 = new Barco();
+    
         //Barco jugador 1;
-        System.out.println("Jugador 1: "+player.player1);
         Limpiar.clean();
         for(i=0;i<4;i++){
-            
+                player1.MostrarTablero(player1.table);
+                System.out.println("Jugador 1: "+player1.Nombre);
                 System.out.println("Colocar barco "+(i+1)+": \n1-Horizontal(hacia a la derecha)\n2-Vertical(hacia abajo)");
                 opc_barco=Integer.parseInt(libro.readLine());
                 Limpiar.clean();
                 barco=false;
             switch(opc_barco){
                 case 1:
-                        
-                        boat.size1=boat.TamañoBarco(boat.ilustracion1,boat.size1,i,boat.nbarco1,boat.barco1);//metodo encontrar el tamaño del barco
+                        player1.MostrarTablero(player1.table);
+                        boat1.size=boat1.TamañoBarco(boat1.ilustracion,boat1.size,i,boat1.nbarco,boat1.barco);//metodo encontrar el tamaño del barco
                         Limpiar.clean();
                         while(!barco){ 
-                            player.MostrarTablero(player.table1);        
-                            c.coor(c.coordenadas1,i);//Llamado al metodo de pedir coordenadas
-                            barco=player.BarcoH(player.table1,boat.size1,c.x,c.y,i,boat.direccion1);//llamado al metodo para guardar barco en horizontal
-                            player.MostrarTablero(player.table1);//llamado al metodo para mostrar tablero
-                            
+                            System.out.println("Tablero de Barcos del Jugador "+player1.Nombre);
+                            player1.MostrarTablero(player1.table);        
+                            boat1.coor(boat1.coordenadasbarco,i);//Llamado al metodo de pedir coordenadas
+                            Limpiar.clean();
+                            barco=player1.BarcoH(player1.table,boat1.size,boat1.x,boat1.y,i,boat1.direccion);//llamado al metodo para guardar barco en horizontal
                         }
                     break;
-                case 2:                     
-                    boat.size1=boat.TamañoBarco(boat.ilustracion1,boat.size1,i,boat.nbarco1,boat.barco1);  
+                case 2:     
+                        player1.MostrarTablero(player1.table);                
+                        boat1.size=boat1.TamañoBarco(boat1.ilustracion,boat1.size,i,boat1.nbarco,boat1.barco);  
                         Limpiar.clean();                      
                         while(!barco){
-                            player.MostrarTablero(player.table1);                           
-                            c.coor(c.coordenadas1,i);//Llamado la metodo de pedir coordenadas
-                            barco=player.BarcoV(player.table1,boat.size1,c.x,c.y,i,boat.direccion1);//llamado al metodo para guardar barco en vertical 
-                            player.MostrarTablero(player.table1);//llamado al metodo para mostrar tablero
-                        }
-                    break;
-                default:
-                    System.out.println("Ingrese una opcion valida mostrada");
-                    i--;
-                    break;
-            }opc_barco=0; //Fin del switch
-        }//fin del for 
-         
-
-       //barco jugador 2
-        System.out.println("Jugador 2: "+player.player2);
-        for(i=0;i<4;i++){
-                System.out.println("Colocar barco "+(i+1)+": \n1-Horizontal(hacia a la derecha)\n2-Vertical(hacia abajo)");
-                opc_barco=Integer.parseInt(libro.readLine());
-                Limpiar.clean();
-                barco=false;
-            switch(opc_barco){
-                case 1:
-                        boat.size2=boat.TamañoBarco(boat.ilustracion2,boat.size2,i,boat.nbarco2,boat.barco2);//metodo encontrar el tamaño del barco
-                        Limpiar.clean();
-                        while(!barco){
-                            player.MostrarTablero(player.table2); 
-                            c.coor(c.coordenadas2,i);//Llamado al metodo de pedir coordenadas
-                            barco=player.BarcoH(player.table2,boat.size2,c.x,c.y,i,boat.direccion2);//llamado al metodo para guardar barco en horizontal
-                            player.MostrarTablero(player.table2);//llamado al metodo para mostrar tablero
-                        }
-                    break;
-                case 2:
-                        boat.size2=boat.TamañoBarco(boat.ilustracion2,boat.size2,i,boat.nbarco2,boat.barco2);//metodo encontrar el tamaño del barco
-                        Limpiar.clean();
-                        while(!barco){
-                            player.MostrarTablero(player.table2); 
-                            c.coor(c.coordenadas2,i);//Llamado al metodo de pedir coordenadas
-                            barco=player.BarcoV(player.table2,boat.size2,c.x,c.y,i,boat.direccion2);//llamado al metodo para guardar barco en horizontal
-                            player.MostrarTablero(player.table2);//llamado al metodo para mostrar tablero
+                            System.out.println("Tablero de Barcos del Jugador "+player1.Nombre);
+                            player1.MostrarTablero(player1.table);                           
+                            boat1.coor(boat1.coordenadasbarco,i);//Llamado la metodo de pedir coordenadas
+                            Limpiar.clean();
+                            barco=player1.BarcoV(player1.table,boat1.size,boat1.x,boat1.y,i,boat1.direccion);//llamado al metodo para guardar barco en vertical 
                         }
                     break;
                 default:
@@ -100,7 +61,51 @@ public class BattleShip {
             }opc_barco=0; //Fin del switch
         }//fin del for 
         
-
+        
+       //barco jugador 2
+        Limpiar.clean();
+        for(i=0;i<4;i++){
+                player1.MostrarTablero(player2.table);
+                System.out.println("Jugador 1: "+player2.Nombre);
+                System.out.println("Colocar barco "+(i+1)+": \n1-Horizontal(hacia a la derecha)\n2-Vertical(hacia abajo)");
+                opc_barco=Integer.parseInt(libro.readLine());
+                Limpiar.clean();
+                barco=false;
+            switch(opc_barco){
+                case 1:
+                        player2.MostrarTablero(player2.table);
+                        boat2.size=boat2.TamañoBarco(boat2.ilustracion,boat2.size,i,boat2.nbarco,boat2.barco);//metodo encontrar el tamaño del barco
+                        Limpiar.clean();
+                        while(!barco){ 
+                            System.out.println("Tablero de Barcos del Jugador "+player2.Nombre);
+                            player2.MostrarTablero(player2.table);        
+                            boat2.coor(boat2.coordenadasbarco,i);//Llamado al metodo de pedir coordenadas
+                            Limpiar.clean();
+                            barco=player2.BarcoH(player2.table,boat2.size,boat2.x,boat2.y,i,boat2.direccion);//llamado al metodo para guardar barco en horizontal
+                        }
+                    break;
+                case 2:     
+                        player2.MostrarTablero(player2.table);                
+                        boat2.size=boat2.TamañoBarco(boat2.ilustracion,boat2.size,i,boat2.nbarco,boat2.barco);  
+                        Limpiar.clean();                      
+                        while(!barco){
+                            System.out.println("Tablero de Barcos del Jugador "+player2.Nombre);
+                            player2.MostrarTablero(player2.table);                           
+                            boat2.coor(boat2.coordenadasbarco,i);//Llamado la metodo de pedir coordenadas
+                            Limpiar.clean();
+                            barco=player2.BarcoV(player2.table,boat2.size,boat2.x,boat2.y,i,boat2.direccion);//llamado al metodo para guardar barco en vertical 
+                        }
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion valida mostrada");
+                    i--;
+                    break;
+            }opc_barco=0; //Fin del switch
+        }//fin del for 
+        
+        boat1.barcos(boat1.size,boat1.coordenadasbarco,boat1.direccion);
+        boat2.barcos(boat2.size,boat2.coordenadasbarco,boat2.direccion);
+        /* 
         do {
         System.out.println("ingrese que jugador quiere que vaya primero. El jugador 1 o el jugador 2 ");
         turnos= Integer.parseInt(libro.readLine());
@@ -138,8 +143,8 @@ public class BattleShip {
     
     
         }while(!fin);
-        boat.barcos(boat.size1,c.coordenadas1,boat.direccion1);
-        boat.barcos(boat.size2,c.coordenadas2,boat.direccion2); 
-
+        boat1.barcos(boat1.size,boat1.coordenadasbarco,boat1.direccion);
+        boat2.barcos(boat2.size,boat2.coordenadasbarco,boat2.direccion);
+        */
     }// fin del static 
 }// fin de la clase 
