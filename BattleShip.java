2efd101dc64b4483c18ff9f5e36c2867a3a0c13a
Martin <martingomez3696@gmail.com>
2023-07-08 21:36:07 -0500

    CODIGO TERMINADO AMEN

--- BattleShip.java
+++ BattleShip.java
@@ -293,26 +293,6 @@ public class BattleShip {
                     libro.readLine();
                 }// fin de turno jugador 2
 
-<<<<<<< HEAD
-                //que barco se hundio 
-                if (c.todosBarcosHundidos(player.table1)) {
-                    System.out.println("Todos los barcos del jugador 1 se han hundido");
-                    System.out.println("GANADOR -> Jugador 2: " + pJugador);
-                    boat.ListaBarcos(boat.size1, c.coordenadas1, boat.direccion1);
-                    System.out.println("Los barcos de" +player.player1 + " eran");
-                    boat.ListaBarcos(boat.size1, c.coordenadas1, boat.direccion1);
-                    FindelJuego = true;
-
-                }
-                if (c.todosBarcosHundidos(player.table2)) {
-                    System.out.println("Todos los barcos del jugador 2 se han hundido");
-                    System.out.println("GANADOR -> Jugador 1: " + pJugador);
-                    boat.ListaBarcos(boat.size2, c.coordenadas2, boat.direccion2);
-                    System.out.println("Los barcos de " +player.player2 + " eran");
-                    boat.ListaBarcos(boat.size2, c.coordenadas2, boat.direccion2);
-                    FindelJuego = true;
-                }
-=======
             } catch (Exception e) {
                 System.out.println("ingrese una coordenada valida");
             } //fin de try catch  
@@ -342,7 +322,6 @@ public class BattleShip {
                 boat.ListaBarcos(boat.size2, c.coordenadas2, boat.direccion2);
                 FindelJuego = true;
             }//fin de if
->>>>>>> 10aaa613ee4f6062e546830d60bcb3e0d345b946
 
             
         } while (!FindelJuego); //fin de do while
